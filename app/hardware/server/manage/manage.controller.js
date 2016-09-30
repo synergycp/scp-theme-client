3181fbd148b8fead7653d0683a67e5e28b7f695c
Zane Hooper <zanehoop@gmail.com>
2016-09-30 02:22:02 -0400

    Bandwidth fix

--- app/hardware/server/manage/manage.controller.js
+++ app/hardware/server/manage/manage.controller.js
@@ -16,7 +16,7 @@
   function ServerManageCtrl(
     Api,
     EventEmitter,
-    BandwidthFilter,
+    ServerManagePanelBandwidth,
     date,
     moment,
     $stateParams,
@@ -36,10 +36,6 @@
     EventEmitter().bindTo(vm.server);
     panelContext.server = vm.server;
 
-    var bandwidth = _.assign({
-      filter: BandwidthFilter(),
-    }, panelContext);
-
     vm.panels = {
       top: [],
       left: [],
@@ -53,10 +49,8 @@
     function activate() {
       vm.server
         .load()
-        .then(loadPanels);
-
-      $scope.$on('$routeUpdate', syncStateToFilter);
-      bandwidth.filter.on('change', syncFilterToState);
+        .then(loadPanels)
+        ;
     }
 
     function loadServer() {
@@ -70,14 +64,7 @@
       var defer = $q.defer();
 
       $scope.$evalAsync(function() {
-        // TODO: fix this shit
-        _.assign(vm.server, response, {
-          get: $api.get,
-          all: $api.all,
-          one: $api.one,
-          patch: patchServer,
-          remove: $api.remove,
-        });
+        _.assign(vm.server, response);
 
         defer.resolve(vm.server);
       });
@@ -86,12 +73,9 @@
     }
 
     function loadPanels() {
-      syncStateToFilter();
-
-      _.setContents(vm.panels.top, [{
-        templateUrl: PANELS+'/panel.bandwidth.html',
-        context: bandwidth,
-      }]);
+      _.setContents(vm.panels.top, [
+        ServerManagePanelBandwidth(vm.server, $scope),
+      ]);
 
       _.setContents(vm.panels.left, [{
         templateUrl: PANELS+'/panel.hardware.html',
@@ -114,25 +98,6 @@
         templateUrl: PANELS+'/panel.os-reload.html',
         context: panelContext,
       },]));
-    }
-
-    function syncStateToFilter() {
-      if (!$stateParams['bandwidth.start']) {
-        return;
-      }
-
-      bandwidth.filter.setRange(
-        moment($stateParams['bandwidth.start'], date.formatDateTime),
-        moment($stateParams['bandwidth.end'], date.formatDateTime)
-      );
-    }
-
-    function syncFilterToState() {
-      var filter = bandwidth.filter;
-      $state.go($state.current.name, _.assign($stateParams, {
-        'bandwidth.start': filter.start.format(date.formatDateTime),
-        'bandwidth.end': filter.end.format(date.formatDateTime),
-      }));
     }
 
     function patchServer() {
