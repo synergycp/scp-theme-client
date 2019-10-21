5d995a93396570727c3e19219ef793dd9670eb4b
natehooper <natehoop@gmail.com>
2019-10-21 19:24:26 -0400

    user-sidebar-updates

--- app/user/user.sidebar.js
+++ app/user/user.sidebar.js
@@ -1,6 +1,21 @@
 (function () {
   'use strict';
 
+  var MY_ACCOUNT = {
+    text: "My Account",
+    sref: "app.account.settings",
+  };
+
+  var SUB_CLIENTS = {
+    text: "Sub Clients",
+    sref: "app.user.client.sub.list",
+  };
+
+  var SUPER_CLIENTS = {
+    text: "Super Clients",
+    sref: "app.user.client.super.list",
+  };
+
   angular
     .module('app.user')
     .config(NavConfig)
@@ -9,20 +24,37 @@
   /**
    * @ngInject
    */
-  function NavConfig(NavProvider) {
-    NavProvider.group('user', {
+  function NavConfig(Auth, NavProvider, Permission) {
+    //Need to import Auth and Permission here. Why doesn't this work? in install.nav.config.js and search.tab.js
+    //this is used in seemingly the exact same way.
+    var group = NavProvider.group('user', {
       translate: "nav.USERS",
       sref: "app.account.settings",
       icon: "fa fa-user",
-    }).item({
-      text: "My Account",
-      sref: "app.account.settings",
-    }).item({
-      text: "Sub Clients",
-      sref: "app.user.client.sub.list",
-    }).item({
-      text: "Super Clients",
-      sref: "app.user.client.super.list"
     });
+
+    Auth.whileLoggedIn(show, hide);
+
+    function show() {
+      Permission
+        .map()
+        .then(showPermitted)
+      ;
+    }
+
+    function showPermitted(map) {
+      group.item(MY_ACCOUNT);
+
+      // if (showSubAndSuperClientPages) {
+        group.item(SUB_CLIENTS);
+        group.item(SUPER_CLIENTS);
+      // }
+    }
+
+    function hide() {
+      group.remove(MY_ACCOUNT);
+      group.remove(SUB_CLIENTS);
+      group.remove(SUPER_CLIENTS);
+    }
   }
 })();
