0e8f8b4f897bf40a1f2fd99030d73efd2e762095
Zane Hooper <zanehoop@gmail.com>
2020-10-09 20:36:54 -0400

    Testing Pipelines

--- bin/build-and-export.sh
+++ bin/build-and-export.sh
@@ -6,5 +6,6 @@ exit_with_error() {
   exit $CODE
 }
 
+mkdir -p ../build
 ./node_modules/.bin/gulp prod build || exit_with_error "Gulp build failed"
 tar -zcvf "../build/client.tar.gz" --transform 's,^public,client,' public || exit_with_error "Archive build failed"
