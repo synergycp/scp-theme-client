fb1c06e35a4633dd389778ef336cbf1a1993896c
Daniel Esteves <danielzesteves@gmail.com>
2025-11-26 16:28:39 -0300

    Fix deploy

--- bin/build-and-export.sh
+++ bin/build-and-export.sh
@@ -9,7 +9,6 @@ exit_with_error() {
 mkdir -p build
 ./node_modules/.bin/gulp prod build || exit_with_error "Gulp build failed"
 
-# Eliminar node_modules antes de crear el tar
 find public/vendor -type d -name 'node_modules' -exec rm -rf {} + 2>/dev/null || true
 
 tar -zcvf "build/client.tar.gz" \
