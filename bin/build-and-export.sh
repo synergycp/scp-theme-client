f26c4379b54ef1a37bc32922bf9a41d745da2dd7
Daniel Esteves <danielzesteves@gmail.com>
2025-11-28 11:32:09 -0300

    REvert changes build and export

--- bin/build-and-export.sh
+++ bin/build-and-export.sh
@@ -8,9 +8,4 @@ exit_with_error() {
 
 mkdir -p build
 ./node_modules/.bin/gulp prod build || exit_with_error "Gulp build failed"
-
-find public/vendor -type d -name 'node_modules' -exec rm -rf {} + 2>/dev/null || true
-
-tar -zcvf "build/client.tar.gz" \
-  --transform 's,^public,client,' \
-  public || exit_with_error "Archive build failed"
+tar -zcvf "build/client.tar.gz" --transform 's,^public,client,' public || exit_with_error "Archive build failed"
